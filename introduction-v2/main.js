8670631e366954dc2714f0ccd5a02e9ceb91428c
Viktor <work.morpi@gmail.com>
2020-08-05 13:58:29 +0300

    updates

--- introduction-v2/main.js
+++ introduction-v2/main.js
@@ -1,9 +1,17 @@
 let collapse = document.querySelector(".introduction__collapse");
 
 collapse.onclick = function (e) {
-	if (e.target.classList.contains("introduction__toggler")) {
-		e.target.classList.toggle("open");
-	} else {
-		e.target.parentNode.parentNode.classList.toggle("open");
-	}
+  if (e.target.classList.contains("introduction__toggler")) {
+    e.target.classList.toggle("open");
+  } else {
+    e.target.parentNode.parentNode.classList.toggle("open");
+  }
 };
+
+var mySwiper = new Swiper(".swiper-container-introduction", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
