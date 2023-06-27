a8493eff3f1285514becda81f31c61d4646c49e4
Viktor Hordynskyi <vhordy@softserveinc.com>
2023-06-27 20:02:13 +0300

    fuseprop update 27.06.2023

--- FuseProp/js/authentication.js
+++ FuseProp/js/authentication.js
@@ -2,9 +2,9 @@ const checkLoggedIn = () => {
     let isLoggedIn = Cookies.get('logged');
 
     if (!isLoggedIn && !window.location.pathname.includes('login')) {
-        window.location.replace("../login.html");
+        window.location.replace("https://www.fuseprop.com/demo/login.html");
     } else if (isLoggedIn && window.location.pathname.includes('login')) {
-        window.location.replace("../pages/home.html");
+        window.location.replace("https://www.fuseprop.com/demo/pages/home.html");
     }
 };
 checkLoggedIn();
